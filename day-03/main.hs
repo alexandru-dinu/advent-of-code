f6ce654a519f0219fa6174a439e7d1f650077ee4
Alexandru Dinu <alex.dinu07@gmail.com>
2020-12-13 13:47:11 +0200

    [day-03] haskell: consistent (dy, dx) notation

--- day-03/main.hs
+++ day-03/main.hs
@@ -9,15 +9,11 @@ type Pos   = (Int, Int)
 type Slope = (Int, Int)
 
 slopes :: [Slope]
-slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-
-
-at :: Grid -> Pos -> Char
-at grid (i, j) = (grid !! j) !! i
+slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)] -- (dy, dx)
 
 -- Compute the next position
 next :: Slope -> Int -> Pos -> Pos
-next (dx, dy) cols (x, y) = ((x + dx) `mod` cols, y + dy)
+next (dy, dx) cols (y, x) = (y + dy, (x + dx) `mod` cols)
 
 count :: Grid -> Slope -> Int
 count grid slope = length $ filter (== '#') trail
@@ -26,9 +22,9 @@ count grid slope = length $ filter (== '#') trail
         cols = length (head grid)
         -- iterate `next` from (0, 0) to the bottom of the grid
         -- tail because we want to skip (0, 0) from the final list
-        positions = tail $ takeWhile (\(x, y) -> y < rows) $ iterate (next slope cols) (0, 0)
+        positions = tail $ takeWhile (\(y, x) -> y < rows) $ iterate (next slope cols) (0, 0)
         -- get grid chars at those positions
-        trail = map (at grid) positions
+        trail = map (\(y, x) -> (grid !! y) !! x) positions
 
 
 main :: IO ()
