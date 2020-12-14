f1ab0ef5609b8719f5e57a3f7cfca5bc33caf825
Alexandru Dinu <alex.dinu07@gmail.com>
2020-12-15 00:59:43 +0200

    [day-10] + p2 in python (+explain); refactor p1

--- day-10/main.hs
+++ day-10/main.hs
@@ -2,18 +2,26 @@ module Main where
 
 import System.Environment (getArgs)
 import Text.Printf (printf)
-import Data.List (sort, partition)
+import Data.List (sort, group)
+import Data.Maybe (fromMaybe)
+import qualified Data.Map as Map
 
 
 -- Greedily select the next adapter with the min difference
 -- (if there are multiple possibilities)
-getDifferenceProduct :: [Int] -> Int
-getDifferenceProduct xs = d1 * d3
+getDifferenceCount :: [Int] -> Map.Map Int Int
+getDifferenceCount xs = Map.fromList gs
     where
         ss = [0] ++ sort xs ++ [maximum xs + 3] -- add source (0) and destination (max + 3)
-        zs = partition (== 1) $ map (uncurry (-)) $ zip (tail ss) (init ss)
-        d1 = (length . fst) zs
-        d3 = (length . snd) zs
+        gs = map (\x -> (head x, length x)) $ (group . sort . map (uncurry (-))) $ zip (tail ss) (init ss)
+
+
+part1 :: [Int] -> Int
+part1 xs = d1 * d3
+    where
+        counts = getDifferenceCount xs
+        d1 = fromMaybe 0 $ Map.lookup 1 counts
+        d3 = fromMaybe 0 $ Map.lookup 3 counts
 
 
 main :: IO ()
@@ -21,4 +29,5 @@ main = do
     args <- getArgs -- IO [String]
     content <- readFile (head args) -- IO String
     let xs = map read (lines content) :: [Int]
-    printf "Part 1: %d\n" $ getDifferenceProduct xs
+    printf "Part 1: %d\n" $ part1 xs
+    printf "difference counts: %s\n" $ show $ Map.toList $ getDifferenceCount xs
--- day-10/main.py
+++ day-10/main.py
@@ -1,7 +1,10 @@
 import sys
 
-def solve(xs: set, src: int):
+
+def get_difference_product(zs: set):
+    xs = zs.copy()
     d1 = d3 = 0
+    src = 0
 
     while len(xs) > 0:
         # if there are multiple possibilities, greedily select min
@@ -13,11 +16,78 @@ def solve(xs: set, src: int):
         d3  += (diff == 3)
         src  = adapter
 
+    # + 1 for own device which always has a diff of 3
     return d1 * (d3 + 1)
+
+
+def enumerate_combinations(xs: set):
+    def _inner(zs: set, current: int, target: int, acc: set, cs: list):
+        if current == target:
+            cs.append(acc.copy())
+            return
+
+        for a in zs & {current + 1, current + 2, current + 3}:
+            zs.remove(a)
+            acc.add(a)
+
+            _inner(zs, a, target, acc, cs)
+
+            acc.remove(a)
+            zs.add(a)
+
+    cs = []
+    zs = xs.copy()
+    target = max(zs) + 3
+    zs.add(target)
+    _inner(zs, current=0, target=target, acc=set(), cs=cs)
+
+    return cs
+
+
+def count_combinations(xs: set) -> int:
+    """
+               0, 1, 2, 3, 4, 5, ...
+                     [--j--]  i
+     e.g. xs: [0, 1, 4, 5, 6, 7, ...]
+    num_ways: [1, 1, 1, 1, 2, 4, ...]
+
+    there are 4 pos. of reaching from 4 to 7:
+    1) 4 (+1) 5 (+1) 6 (+1) 7
+    2) 4 (+1) 5 (+2) 7
+    3) 4 (+2) 6 (+1) 7
+    4) 4 (+3) 7
+
+    thus: num_ways[5] = num_ways[4] + num_ways[3] + num_ways[2]
+
+    NOTE: looks like a (masked) Tribonacci(len(xs))
+    also similar to Fibonacci staircase (where you can climb 1 or 2 stairs at once)
+    here you can "move" 3 steps at once, so just count the number of ways to get to those steps:
+    4 -> 7 (+3)
+    5 -> 7 (+2)
+    6 -> 7 (+1)
+    """
+    zs = [0, *sorted(xs), max(xs) + 3]
+
+    # num_ways[i] = # of combinations up to size i
+    num_ways = [0] * len(zs)
+    num_ways[0] = 1
+
+    for i in range(1, len(zs)):
+        # look back a window s.t. diff <= 3
+        # add the number of ways to get there
+        # explicitly verbose
+        num_ways[i] = num_ways[i-1] * (zs[i] - zs[i-1] <= 3) + \
+                      num_ways[i-2] * (zs[i] - zs[i-2] <= 3) + \
+                      num_ways[i-3] * (zs[i] - zs[i-3] <= 3)
+
+    return num_ways[-1]
 
 
 if __name__ == "__main__":
     with open(sys.argv[1], 'rt') as fp:
         xs = set([int(x.strip()) for x in fp.readlines()])
 
-    print(f'Part 1: {solve(xs, src=0)}')
+    print(f'Part 1: {get_difference_product(xs)}')
+    print(f'Part 2: {count_combinations(xs)}')
+
+    # print(enumerate_combinations(xs))
