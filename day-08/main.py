a12fa74c710e32be2a528bf804d63fc30065a995
Alexandru Dinu <alex.dinu07@gmail.com>
2020-12-14 15:07:55 +0200

    [day-08] add part-2 solution in python

--- day-08/main.py
+++ day-08/main.py
@@ -1,9 +1,13 @@
 import sys
+from typing import List, Tuple, NewType
+
+InstructionType = NewType('InstructionType', Tuple[str, str]) # e.g. jmp +2
+CodeType = NewType('CodeType', List[InstructionType])
 
 class Evaluator:
     valid_instructions = {'nop', 'acc', 'jmp'}
 
-    def __init__(self, code: list):
+    def __init__(self, code: CodeType):
         self.code = code
         self.ACC  = 0
         self.IP   = 0
@@ -13,7 +17,7 @@ class Evaluator:
         self.__ips.add(self.IP)
         self.IP += delta
 
-    def run_until_loop(self) -> int:
+    def run(self) -> Tuple[bool, int]:
         while self.IP < len(self.code):
             inst, arg = self.code[self.IP]
             delta = 1 # normally, go to next instruction
@@ -28,18 +32,49 @@ class Evaluator:
                 delta = int(arg)
                 next_ip = self.IP + delta
                 if next_ip in self.__ips: # loop found
-                    break
+                    return False, self.ACC # False = early termination
 
             else:
                 raise ValueError(f'Invalid instruction {inst} {arg}')
 
             self.__advance(delta)
 
-        return self.ACC
+        return True, self.ACC
+
+
+def repair(code: CodeType) -> CodeType:
+    def flip(loc: InstructionType) -> InstructionType:
+        inst, arg = loc
+
+        if inst == 'jmp':
+            return 'nop', arg
+        if inst == 'nop':
+            return 'jmp', arg
+
+        raise ValueError(f'Invalid instruction {inst} {arg}')
+
+    for i, (inst, arg) in enumerate(code):
+        if inst == 'acc':
+            continue
+
+        code[i] = flip(code[i])
+        status, acc = Evaluator(code).run()
+        if status == True: # loop was fixed
+            return code
+        code[i] = flip(code[i])
+
+    # unreachable
+    assert False
 
 
 if __name__ == "__main__":
     with open(sys.argv[1], 'rt') as fp:
-        code = [tuple(l.strip().split()) for l in fp.readlines()]
+        code: CodeType = [tuple(l.strip().split()) for l in fp.readlines()]
 
-    print(f'Part 1: {Evaluator(code).run_until_loop()}')
+    status, acc = Evaluator(code).run()
+    assert status == False # loop has to be found
+    print(f'Part 1: {acc}')
+
+    status, acc = Evaluator(repair(code)).run()
+    assert status == True # loop was fixed
+    print(f'Part 2: {acc}')
