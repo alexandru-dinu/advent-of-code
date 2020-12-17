b623de2ba160f24bc0b141329b154fb8edba889f
Alexandru Dinu <alex.dinu07@gmail.com>
2020-12-17 12:02:05 +0200

    [day-13] solve part-2

--- day-13/main.m
+++ day-13/main.m
@@ -1,9 +1,50 @@
 xs = strsplit(fileread(argv(){1}), '\n');
-e  = str2num(xs{1});
-bs = strsplit(xs{2}, ',');
+earliest = str2num(xs{1});
+busids = strsplit(xs{2}, ',');
 
-mask = cellfun(@(x) ~isequal(x, 'x'), bs);
-bids = cellfun(@(x) str2num(x), bs(mask));
+rs = []; % remainders
+ms = []; % moduli (bus ids)
+for i = 1:length(busids)
+    if busids{i} ~= 'x'
+        m = str2num(busids{i});
+        ms = [ms, m];
+        rs = [rs, mod(-(i-1), m)];
+    end
+end
 
-[tmin, imin] = min(arrayfun(@(b) b * ceil(e / b), bids));
-printf("Part 1: %d\n", bids(imin) * (tmin - e))
+
+[tmin, imin] = min(arrayfun(@(b) b * ceil(earliest / b), ms));
+printf("Part 1: %d\n", ms(imin) * (tmin - earliest));
+
+
+% TODO: smarter
+function x = modinv(a, m)
+    for x = 1:m
+        if mod(a * x, m) == 1
+            return;
+        end
+    end
+endfunction
+
+
+function t = crt(rs, ms)
+    M = prod(ms);
+
+    t = 0;
+    for i = 1:length(rs)
+        q = idivide(M, int64(ms(i)), 'fix');
+        t += rs(i) * q * modinv(q, ms(i));
+    end
+
+    t = mod(t, M);
+endfunction
+
+
+xs = (0 <= rs < ms);
+assert(sum(xs == 1) == length(xs), "Remainder must be in [0, modulus)!");
+
+xs = nchoosek(ms, 2);
+xs = gcd(xs(:, 1), xs(:, 2));
+assert(sum(xs == 1) == length(xs), "Moduli must be pairwise coprime!");
+
+printf("Part 2: %d\n", crt(rs, ms));
