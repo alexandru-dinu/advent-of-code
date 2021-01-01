c24f8e3a8b94d12d419ae1a47350e26cd0b7bc3e
Alexandru Dinu <alex.dinu07@gmail.com>
2021-01-01 19:15:10 +0200

    [structure] reformat files to 2 spaces

--- 2019/day-01/Main.scala
+++ 2019/day-01/Main.scala
@@ -3,19 +3,19 @@ import scala.io.Source
 
 object Main {
 
-    def main(args: Array[String]) = {
-        val xs = Source.fromFile(args(0))
-                       .getLines()
-                       .filter(!_.isEmpty())
-                       .map(line => line.toInt)
-                       .toList
+  def main(args: Array[String]) = {
+    val xs = Source.fromFile(args(0))
+                   .getLines()
+                   .filter(!_.isEmpty())
+                   .map(line => line.toInt)
+                   .toList
 
-        val step = (x: Int) => x / 3 - 2
+    val step = (x: Int) => x / 3 - 2
 
-        val p1 = xs.map(step).sum
-        println(s"Part 1: $p1")
+    val p1 = xs.map(step).sum
+    println(s"Part 1: $p1")
 
-        val p2 = xs.map(Iterator.iterate(_)(step).takeWhile(_ > 0).drop(1).sum).sum
-        println(s"Part 2: $p2")
-    }
+    val p2 = xs.map(Iterator.iterate(_)(step).takeWhile(_ > 0).drop(1).sum).sum
+    println(s"Part 2: $p2")
+  }
 }
