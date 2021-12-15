168b0dcb5fcb6f25bce613b7311bf70ca6115c1d
Alexandru Dinu <alex.dinu07@gmail.com>
2021-12-16 00:31:50 +0200

    [2021/12] (#5) simplify and upsolve

--- 2021/day-12/README.md
+++ 2021/day-12/README.md
@@ -1,9 +1,21 @@
 ## [Day 12: Passage Pathing](https://adventofcode.com/2021/day/12)
 
 ### Solution
+
+#### Upsolving
+- Using a "can visit twice" boolean flag for small caves (see this [Reddit post](https://www.reddit.com/r/adventofcode/comments/rehj2r/2021_day_12_solutions/ho7ufss/)),
+part 2 is greatly simplified.
+- The general idea is the following:
+  - Big nodes don't count as visited.
+  - Small nodes are either:
+    - visited once, passing the flag further.
+    - visited twice (i.e. during the first visit, they are not accounted for).
+
+#### Old solution
 - Maintain a `can_visit[node]` count telling how many times `node` can be visited (e.g. `inf` for big caves, `1` for small caves).
 - Part 1 is just DFS.
-- (_A bit suboptimal_) For part 2, for each small caves, allow them to be visited twice, then apply DFS. Finally accumulate all the unique paths.
+- (_A bit suboptimal_) For part 2, for each small caves, allow them to be visited twice, then apply DFS.
+Finally accumulate all the unique paths.
 ```
 Part 1: 4241
 Part 2: 122134
--- 2021/day-12/solve.py
+++ 2021/day-12/solve.py
@@ -1,105 +1,42 @@
+from __future__ import annotations
+
 import sys
 from collections import defaultdict
-from typing import *
-
-import numpy as np
-
-Graph = Dict[str, List[str]]
 
 
-def node_type(node: str) -> str:
-    if node in ["start", "end"]:
-        return "special"
+def dfs(graph: dict[str, set[str]], acc: list[str], vis: set[str], twice: bool) -> set:
+    cur = acc[-1]
 
-    if node.islower():
-        return "small"
+    if cur == "end":
+        return {tuple(acc)}
 
-    return "big"
+    paths = set()
 
-
-def init_can_visit(graph: Graph) -> Dict[str, float]:
-    can_visit = {}
-
-    for node in graph:
-        if node == "start":
-            can_visit[node] = 0
-        elif node.isupper():
-            can_visit[node] = np.inf
+    for nei in graph[cur] - vis:
+        if nei.isupper():
+            # don't mark big node as visited
+            paths |= dfs(graph, [*acc, nei], vis, twice)
         else:
-            can_visit[node] = 1
+            # visit small node, passing "twice" further
+            paths |= dfs(graph, [*acc, nei], vis | {nei}, twice)
+            if twice:
+                # allow small node to be visited twice
+                paths |= dfs(graph, [*acc, nei], vis, twice=False)
 
-    return can_visit
-
-
-def dfs(graph, cur, target, acc, paths, can_visit):
-    if cur == target:
-        paths.add(tuple(acc))
-        return
-
-    for nei in set(graph[cur]):
-        if can_visit[nei] == 0:
-            continue
-
-        can_visit[nei] -= 1
-        dfs(graph, nei, target, [*acc, nei], paths, can_visit)
-        can_visit[nei] += 1
-
-
-def part1(graph: Graph):
-    """
-    Count paths that visit small caves at most once.
-    """
-    paths = set()
-
-    dfs(
-        graph,
-        cur="start",
-        target="end",
-        acc=["start"],
-        paths=paths,
-        can_visit=init_can_visit(graph),
-    )
-
-    return len(paths)
-
-
-def part2(graph: Graph):
-    """
-    Count paths that visit a single small cave <=2, other small caves <=1, big caves *.
-    """
-    paths = set()
-
-    can_visit = init_can_visit(graph)
-
-    for node in graph:
-        if node_type(node) != "small":
-            continue
-
-        can_visit[node] += 1
-        dfs(
-            graph,
-            cur="start",
-            target="end",
-            acc=["start"],
-            paths=paths,
-            can_visit=can_visit,
-        )
-        can_visit[node] -= 1
-
-    return len(paths)
+    return paths
 
 
 def main():
     with open(sys.argv[1]) as fp:
         edges = [l.strip().split("-") for l in fp.readlines()]
 
-    graph = defaultdict(lambda: [])
+    graph = defaultdict(lambda: set())
     for u, v in edges:
-        graph[u].append(v)
-        graph[v].append(u)
+        graph[u].add(v)
+        graph[v].add(u)
 
-    print("Part 1:", part1(graph))
-    print("Part 2:", part2(graph))
+    print("Part 1:", len(dfs(graph, acc=["start"], vis={"start"}, twice=False)))
+    print("Part 2:", len(dfs(graph, acc=["start"], vis={"start"}, twice=True)))
 
 
 if __name__ == "__main__":
