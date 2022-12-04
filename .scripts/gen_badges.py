a9c7c2af7ec5f5160e3cafb4a095d9ed6e5dae81
Alexandru Dinu <alex.dinu07@gmail.com>
2022-12-05 00:34:11 +0200

    [misc] remove matplotlib & numpy dependencies

--- .scripts/gen_badges.py
+++ .scripts/gen_badges.py
@@ -3,11 +3,19 @@
 import json
 import os
 import time
+from math import sqrt
 
-import matplotlib as mpl
-import numpy as np
 import requests
-from tqdm import trange
+
+
+def rgb2hex(r, g, b):
+    f = lambda x: max(0, min(255, round(x * 255)))
+    return f"{f(r):2x}{f(g):2x}{f(b):2x}"
+
+
+def hex2rgb(x):
+    return (int(x[:2], 16) / 255, int(x[2:4], 16) / 255, int(x[4:], 16) / 255)
+
 
 SID = os.getenv("AOC_SESSION")
 assert SID is not None
@@ -15,17 +23,20 @@ assert SID is not None
 UID = os.getenv("AOC_UID")
 assert UID is not None
 
-C_RED = np.array(mpl.colors.to_rgb("#ef0f14"))
-C_GRN = np.array(mpl.colors.to_rgb("#239323"))
-
 AOC_URL = "https://adventofcode.com/{year}/leaderboard/private/view/{uid}.json"
 MD_BADGE_URL = "https://img.shields.io/badge/{year}-{stars}%20stars-{color}"
+
+README_PATH = "./README.md"
+
+C_RED = hex2rgb("ef0f14")
+C_GRN = hex2rgb("239323")
 
 
 def get_md_urls():
     out = []
 
-    for year in trange(2022, 2014, -1):
+    for year in range(2022, 2014, -1):
+        print(f"Fetch data for {year=}")
         res = requests.get(AOC_URL.format(year=year, uid=UID), cookies={"session": SID})
         assert res.status_code == 200
         time.sleep(2)
@@ -33,16 +44,17 @@ def get_md_urls():
         data = json.loads(res.text)
 
         s = data["members"][UID]["stars"]
-        t = np.sqrt(s / 50)
-        c = mpl.colors.to_hex((1 - t) * C_RED + t * C_GRN).strip("#")
+        t = sqrt(s / 50)  # sqrt(x) > x, for x in [0, 1], so we get to green faster
+        color = rgb2hex(*[(1 - t) * C_RED[c] + t * C_GRN[c] for c in range(3)])
 
-        md_str = f"[![]({MD_BADGE_URL.format(year=year, stars=s, color=c)})](./{year})"
-        out.append(md_str)
+        out.append(
+            f"[![]({MD_BADGE_URL.format(year=year, stars=s, color=color)})](./{year})"
+        )
 
     return out
 
 
-with open("README.md", "rt") as fp:
+with open(README_PATH, "rt") as fp:
     lines = [x.strip() for x in fp]
 
 for i, line in enumerate(lines):
@@ -53,6 +65,8 @@ for i, line in enumerate(lines):
 
 out = lines[: start + 1] + get_md_urls() + lines[end:]
 
-with open("README.md", "wt") as fp:
+with open(README_PATH, "wt") as fp:
     for line in out:
         fp.write(f"{line}\n")
+
+print(f"{README_PATH} updated!")
