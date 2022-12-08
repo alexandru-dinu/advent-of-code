60ffd91ae36224b48f3dcc215868749e060e79a0
Alexandru Dinu <alex.dinu07@gmail.com>
2022-12-08 22:35:35 +0200

    [2022/08] solve

---
+++ 2022/day-08/README.md
@@ -0,0 +1,16 @@
+## [Day 8: Treetop Tree House](https://adventofcode.com/2022/day/8)
+
+### Solution
+```
+Part 1: 1870
+Part 2: 517440
+```
+Yield the up / down / left / right views and solve solve both parts in one traversal,
+i.e. for each inner tree:
+- part 1: count cases s.t. there's `any` view where `all` other trees are shorter than it
+- part 2: multiply the number of visible trees from `all` views, then find the max
+
+### Usage
+```
+$ make
+```
---
+++ 2022/day-08/solve.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def view_gen(xss: np.ndarray):
+    h, w = xss.shape
+
+    for i in range(1, h - 1):
+        for j in range(1, w - 1):
+            u = xss[i, j] > xss[:i, j][::-1]
+            d = xss[i, j] > xss[i + 1 :, j]
+            l = xss[i, j] > xss[i, :j][::-1]
+            r = xss[i, j] > xss[i, j + 1 :]
+
+            yield u, d, l, r
+
+
+def count_visible(mask):
+    return mask.sum() if mask.all() else 1 + mask.argmin()
+
+
+def main():
+    with open(0) as fp:
+        xss = np.array([[int(x) for x in xs.strip()] for xs in fp])
+
+    part1 = 2 * np.sum(xss.shape) - 4  # start from the border
+    part2 = -1
+
+    for view in view_gen(xss):
+        part1 += any(map(all, view))
+        part2 = max(part2, np.product([count_visible(mask) for mask in view]))
+
+    print("Part 1:", part1)
+    print("Part 2:", part2)
+
+
+if __name__ == "__main__":
+    main()
