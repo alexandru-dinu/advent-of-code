16667e07b11685932b93e0e07ac6fcd4926d259c
Alexandru Dinu <alex.dinu07@gmail.com>
2023-12-06 20:33:56 +0200

    [2018/01] refactor

--- 2018/01/README.md
+++ 2018/01/README.md
@@ -1,10 +1,18 @@
-## [Day 1: Chronal Calibration](https://adventofcode.com/2018/day/1)
+---
+title: "Day 1: Chronal Calibration"
+url: https://adventofcode.com/2018/day/1
+tags: lists
+---
 
 ### Solution
 ```
 Part 1: 435
 Part 2: 245
 ```
+
+Part 1: sum.
+
+Part 2: keep track of the running sum and report first sum which has been previously encountered.
 
 ### Usage
 ```
--- 2018/01/solve.py
+++ 2018/01/solve.py
@@ -1,22 +1,38 @@
 from itertools import cycle
 
-import numpy as np
+
+def part1(xs):
+    return sum(xs)
+
+
+def part2(xs):
+    f = 0
+    seen = {f}
+
+    for i in cycle(range(len(xs))):
+        f += xs[i]
+        if f in seen:
+            return f
+        seen.add(f)
+
+
+def test_example():
+    assert part1([1, 1, 1]) == 3
+    assert part1([1, 1, -2]) == 0
+    assert part1([-1, -2, -3]) == -6
+
+    assert part2([+1, -1]) == 0
+    assert part2([+3, +3, +4, -2, -4]) == 10
+    assert part2([-6, +3, +8, +5, -6]) == 5
+    assert part2([+7, +7, -2, -7, -4]) == 14
 
 
 def main():
     with open(0) as fp:
-        xs = np.array([int(x.strip()) for x in fp])
+        xs = [int(x.strip()) for x in fp]
 
-    print("Part 1:", xs.sum())
-
-    f = 0
-    seen = {f}
-    for i in cycle(range(len(xs))):
-        f += xs[i]
-        if f in seen:
-            print("Part 2:", f)
-            break
-        seen.add(f)
+    print(f"Part 1: {part1(xs)}")
+    print(f"Part 2: {part2(xs)}")
 
 
 if __name__ == "__main__":
