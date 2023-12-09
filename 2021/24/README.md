c57df5879bce36086c7686cc3c128cad9bba711c
Alexandru Dinu <alex.dinu07@gmail.com>
2023-12-09 12:16:01 +0200

    [2021/24] solve

--- 2021/24/README.md
+++ 2021/24/README.md
@@ -1,9 +1,59 @@
-## [Day 24: Arithmetic Logic Unit](https://adventofcode.com/2021/day/24)
+---
+title: "Day 24: Arithmetic Logic Unit"
+url: https://adventofcode.com/2021/day/24
+tags: reverse-engineering, maths
+---
 
 ### Solution
-...
+```
+Part 1: 99999795919456
+Part 2: 45311191516111
+```
+
+Clearly bruteforce is impossible, as there are 9**14 total options. Instead, we can inspect the input and isolate the `inp w` instructions. We see that there are 14 **sub-programs** (each starting with `inp w`), all consisting of 18 instructions.
+
+We further notice that the sub-programs are very similar -- they differ only at 3 instructions.
+All unique instr. for the differing indices (0-based):
+```
+ 3: div z [1, 26]
+ 4: add x [...]
+14: add y [...]
+```
+
+After analysing the structure of a sub-program, we can translate it roughly to:
+```python
+def subprog(w, z=0):
+    # z is always >= 0
+    x = z % 26 + i1
+    z //= i2
+    if x != w:
+        z *= 26
+        z += w + i3
+    return z
+```
+
+This computation looks like manipulating `z` by adding values in base 26.
+Specifically, the code pushes a value (`w + i3`) inside the `if`-block and it pops it when doing `z //= 26`. [^1]
+
+We further notice that `z` is divided by either 1 (no-op) or 26 (right-shift) and that there are 7 of each.
+Therefore, for each `z //= 1` we push a value and for each `z //= 26` we pop a value. Since `#(push) == #(pop)`, we will end up with `z == 0`.
+
+The condition for push is `x != w` and the condition for pop is `x == w` when `z` is divided by 26.
+We can then match the pushed value (when `i2 == 1`), i.e. `w + i3`, to the popped value (when `i2 == 26`), i.e. `z % 26 + i1`.
+When we push a value, `x` will always be `> 9`, so the condition `x != w` will always be true.
+
+The last pushed value is `z % 26`, so the condition becomes:
+```
+x = z % 26 + i1
+x = last_pushed + i1
+x - i1 = w_prev + i3      # we previously pushed w + i3 when we entered `x != w`
+w_cur - i1 = w_prev + i3  # since we want `x == w`
+```
+We can solve this by matching the push indices (where `i2 == 1`) to the corresponding pop indices (where `i2 == 26`).
 
 ### Usage
 ```
 $ make
 ```
+
+[^1]: push/pop hint from: https://www.reddit.com/r/adventofcode/comments/rnejv5/comment/hps5hgw
--- 2021/24/solve.py
+++ 2021/24/solve.py
@@ -1,7 +1,9 @@
-from __future__ import annotations
-
+import itertools as it
+import random
 import sys
-from itertools import product
+
+from rich import print
+from tqdm import trange
 
 
 def run(prog: list[str], inp: str) -> bool:
@@ -16,40 +18,164 @@ def run(prog: list[str], inp: str) -> bool:
         except ValueError:
             cmd, a = inst.split()
 
-        if cmd == "inp":
-            vs[a] = int(next(inp))
-        elif cmd == "add":
-            vs[a] += vs[b] if b in vs else int(b)
-        elif cmd == "mul":
-            vs[a] *= vs[b] if b in vs else int(b)
-        elif cmd == "div":
-            vs[a] //= vs[b] if b in vs else int(b)
-        elif cmd == "mod":
-            vs[a] %= vs.get(b, int(b))
-        elif cmd == "eql":
-            lhs = vs[a] if a in vs else int(a)
-            rhs = vs[b] if b in vs else int(b)
-            vs[a] = int(lhs == rhs)
+        match cmd:
+            case "inp":
+                vs[a] = int(next(inp))
+            case "add":
+                vs[a] += vs[b] if b in vs else int(b)
+            case "mul":
+                vs[a] *= vs[b] if b in vs else int(b)
+            case "div":
+                vs[a] //= vs[b] if b in vs else int(b)
+            case "mod":
+                vs[a] %= vs[b] if b in vs else int(b)
+            case "eql":
+                vs[a] = int(vs[a] == (vs[b] if b in vs else int(b)))
+            case _:
+                raise ValueError(f"Unknown {cmd=}")
+
+    return vs
+
+
+def diff(p1, p2):
+    assert len(p1) == len(p2)
+    return [i for i in range(len(p1)) if p1[i] != p2[i]]
+
+
+def rev(w, i1, i2, i3, z):
+    assert z >= 0
+    x = z % 26 + i1
+    z //= i2
+    if x != w:
+        z *= 26
+        z += w + i3
+    return z
+
+
+def run_rev(w):
+    z = 0
+    for _w, i1, i2, i3 in zip(w, I1, I2, I3):
+        z = rev(int(_w), i1, i2, i3, z)
+        print(_w, z)
+    return z
+
+
+def test_rev(prog, niter):
+    for _ in trange(niter):
+        w = "".join(str(random.randint(1, 9)) for _ in range(14))
+        assert run_rev(w) == run(prog, w)["z"]
+
+
+def search(space, pushpop):
+    w = [0] * 14
+    assert len(pushpop) == 7
+    for p in it.product(space, repeat=len(pushpop)):
+        for i, (push, pop) in enumerate(pushpop):
+            w[push] = p[i]
+            w[pop] = I1[pop] + w[push] + I3[push]
+            if not 0 < w[pop] < 10:
+                break
         else:
-            raise ValueError(f"Unknown {cmd=}")
+            break
 
-    return vs.get("z") == 0
+    return "".join(map(str, w))
 
 
-def part1(prog: list[str]) -> str:
-    for inp in product("987654321", repeat=14):
-        inp = "".join(inp)
-        if run(prog, inp):
-            return inp
+I1 = []
+I2 = []
+I3 = []
 
-    assert False, "unreachable"
+
+def populate_Is(subprogs):
+    global I1, I2, I3
+    for sp in subprogs:
+        I2.append(int(sp[3].split()[-1]))
+        I1.append(int(sp[4].split()[-1]))
+        I3.append(int(sp[14].split()[-1]))
+
+
+def find_push_pop(xs):
+    s = []
+    ret = []
+
+    for i, x in enumerate(xs):
+        if x == 1:
+            s.append(i)
+        elif x == 26:
+            j = s.pop()
+            ret.append((j, i))
+        else:
+            assert False
+
+    assert len(s) == 0
+    return sorted(ret, key=lambda xy: xy[0])
 
 
 def main():
     with open(sys.argv[1]) as fp:
         prog = [l.strip() for l in fp.readlines()]
 
-    print("Part 1:", part1(prog))
+    subprogs = []
+    for k, g in it.groupby(prog, key=lambda x: x == "inp w"):
+        if not k:  # not matching 'inp w'
+            subprogs.append(list(g))
+    assert len(subprogs) == 14
+    assert all(len(sp) == 17 for sp in subprogs)
+
+    populate_Is(subprogs)
+    pushpop = find_push_pop(I2)
+
+    if 0:
+        test_rev(prog, 500_000)
+    else:
+        space = list(range(1, 10))
+
+        w = search(space[::-1], pushpop)
+        assert run(prog, w)["z"] == 0
+        print(f"Part 1: {w}")
+
+        w = search(space, pushpop)
+        assert run(prog, w)["z"] == 0
+        print(f"Part 2: {w}")
+
+    exit(0)
+    ################################
+    ## Exploration
+
+    # the only diff. instructions are @ 3, 4, 14
+    for p1, p2 in it.combinations(subprogs, 2):
+        assert set(diff(p1, p2)) <= {3, 4, 14}
+
+    # instr. involving w are @ 5, 13, so they're the same for all subprogs:
+    # eql x w
+    # add y w
+    assert all(
+        {5, 13} == {i for i, inst in enumerate(sp) if "w" in inst} for sp in subprogs
+    )
+
+    # special = {k: defaultdict(list) for k in [3, 4, 14]}
+    # for j, sp in enumerate(subprogs):
+    #     for i in [3, 4, 14]:
+    #         special[i][sp[i]].append(j)
+    # print(special)
+
+    for sp in subprogs:
+        print(sp[4], "|", sp[3], "|", sp[14])
+
+    # import os
+
+    # while True:
+    #     a = int(input())
+    #     b = int(input())
+
+    #     os.system("clear")
+    #     for i, (p1, p2) in enumerate(zip(subprogs[a], subprogs[b])):
+    #         s = f"{i:2d}: {p1:12s} -- {p2:12s}"
+    #         if p1 != p2:
+    #             s = f"[red]{s}"
+    #         if "w" in p1 or "w" in p2:
+    #             s = f"[italic]{s}"
+    #         print(s)
 
 
 if __name__ == "__main__":
